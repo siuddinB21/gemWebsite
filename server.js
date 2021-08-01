26fb69295cc888e1f89d3b1567121df6620d812b
siuddinB21 <syed.iftekharuddin@gmail.com>
2021-08-01 15:05:24 +0500

    make

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const http = require('http');
+const express = require('express');
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end('Hello, World!');
-}
+//var session=require('express-session') ;
 
-const server = http.createServer(requestListener);
-var port = process.env.PORT || 5000;
-server.listen(port);
+
+
+const path = require('path');
+
+const home = require('./controllers/home');
+
+//const requestListener = function (req, res) {
+ //   res.writeHead(200);
+ //   res.end('Hello, World!');
+//}
+
+const staticPath=path.join(__dirname,"/public");
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(express.static(staticPath))
+
+
+//const server = http.createServer(requestListener);
+
+app.listen( port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
+//server.listen(port);
---
+++ views/tetst.js
@@ -0,0 +1,5 @@
+const express=require("express");
+const app =express();
+const port=8000;
+//app.use(express.static(path));
+console.log(__dirname);
